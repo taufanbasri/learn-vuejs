e5344f09d1c40e92f647b49c1c5e1576c03c6f05
taufanbasri <taufanprasetyobasri@gmail.com>
2017-12-18 14:02:51 +0700

    Practical Component Exercise Message -> other way

--- main.js
+++ main.js
@@ -11,19 +11,13 @@ Vue.component('message', {
     <article class="message" v-show="isVisible"> \
       <div class="message-header"> \
         {{ title }} \
-        <button type="button" @click="hideModal">x</button> \
+        <button type="button" @click="isVisible = false">x</button> \
       </div> \
       <div class="message-body"> \
         {{ body }} \
       </div> \
     </article> \
   ',
-
-  methods: {
-    hideModal() {
-      this.isVisible = false;
-    }
-  }
 });
 
 new Vue({
