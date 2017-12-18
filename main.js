f2b6a5e1fc63f08162d4e0387991e847caa2b652
taufanbasri <taufanprasetyobasri@gmail.com>
2017-12-18 12:34:35 +0700

    components within components

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
+Vue.component('task-list', {
+  template: '<div><task v-for="task in tasks">{{ task.task }}</task></div>',
+
+  data() {
+    return {
+      tasks: [
+        { task: 'Reciting Quran', completed: true },
+        { task: 'Read Quran', completed: false },
+        { task: 'Memorize Quran', completed: true },
+        { task: 'Go to Market', completed: false },
+        { task: 'Play Footbal', completed: false }
+      ]
+    }
+  }
+});
+
 Vue.component('task', {
   template: '<li><slot></slot></li>'
 });
